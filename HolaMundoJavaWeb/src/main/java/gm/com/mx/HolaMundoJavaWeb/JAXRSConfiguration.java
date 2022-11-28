d3c1ef4d76e3978bf9afe229256cbb525c8e566a
Alejandro Espinoza Aguilar <ale_1617@outlook.com>
2022-11-28 14:16:14 -0600

    agregando un proyecto

---
+++ HolaMundoJavaWeb/src/main/java/gm/com/mx/HolaMundoJavaWeb/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package gm.com.mx.HolaMundoJavaWeb;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
